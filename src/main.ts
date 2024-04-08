ec264ec5cbad6bdc9302e81a17744310ce7cb780
malemajo <malemajo@asu.edu>
2024-04-08 11:10:29 -0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './modules/app/app.module';
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
+import { InternalServerErrorException } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.use(cookieParser(process.env.COOKIE_SECRET));
+  app.enableCors({
+    origin: function (origin, callback) {
+      const allowedOrigins = ['http://localhost:3000', 'https://fahoot.com', 'https://www.fahoot.com'];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new InternalServerErrorException('Not allowed by CORS'));
+      }
+    },
+    credentials: true, // include this line to allow cookies
+    allowedHeaders: ['Access-Control-Allow-Origin', 'Content-Type', 'Authorization'], // Allow necessary headers
+  });
+
+  await app.listen(process.env.PORT ?? 8080);
+}
+bootstrap().catch((error) => console.error(error));
