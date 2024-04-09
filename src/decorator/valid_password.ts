3bb4db41384f5eee588a08abfc2c9b2b2db90934
malemajo <malemajo@asu.edu>
2024-04-09 02:34:36 -0400

    - Implement user entity and add hooks to make names and email address lowercase - Implement user response dto to shape out-going responses - Implement create user dto to validate user incoming data - Implement a custom decorator to valid password

---
+++ src/decorator/valid_password.ts
@@ -0,0 +1,22 @@
+import { ValidationOptions, registerDecorator } from 'class-validator';
+
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,64}$/;
+
+export function IsValidPassword(validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string): void {
+    registerDecorator({
+      name: 'isValidPassword',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: any, args): boolean {
+          return typeof value === 'string' && PWD_REGEX.test(value); // You can return a Promise<boolean> here as well, if you want to make async validation
+        },
+        defaultMessage(args): string {
+          return `${args.property} must be a valid password. It must contain at least one uppercase letter, one lowercase letter, one number, and one special character (!@#$%). It must also be 8-64 characters long.`;
+        },
+      },
+    });
+  };
+}
---
+++ src/modules/user/dto/user_response.dto.ts
@@ -0,0 +1,26 @@
+// user-response.dto.ts
+
+import { AuthenticationType } from '../../../utils/constant';
+import { User } from '../entity/user.entity';
+
+export class UserResponseDto {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  verified: boolean;
+  authenticationType: AuthenticationType;
+  createdAt: Date;
+  updatedAt: Date;
+
+  constructor(user: User) {
+    this.id = user.id;
+    this.firstName = user.firstName;
+    this.lastName = user.lastName;
+    this.email = user.email;
+    this.verified = user.verified;
+    this.authenticationType = user.authenticationType;
+    this.createdAt = user.createdAt;
+    this.updatedAt = user.updatedAt;
+  }
+}
