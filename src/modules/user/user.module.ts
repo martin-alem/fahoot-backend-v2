2e7a6123520fc6e803831880229d2110d2f17edf
malemajo <malemajo@asu.edu>
2024-04-09 19:12:56 -0400

    - Implement api key middleware to prevent arbitrary client from making request

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { SharedModule } from '../shared/shared.module';
 import { NotificationModule } from '../notification/notification.module';
+import { APIKeyMiddleware } from '../../middleware/api_key.middleware';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User]), SharedModule, NotificationModule],
@@ -12,4 +13,8 @@ import { NotificationModule } from '../notification/notification.module';
   controllers: [UserController],
   exports: [UserService]
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(APIKeyMiddleware).forRoutes(UserController);
+  }
+}
